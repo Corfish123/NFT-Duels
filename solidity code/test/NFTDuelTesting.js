ac71fe8a9601ea239b734203c748f6ae50510dbe
Corfish123 <80220215+Corfish123@users.noreply.github.com>
2022-06-01 09:43:58 -0400

    import open zepplin testing

--- solidity code/test/NFTDuelTesting.js
+++ solidity code/test/NFTDuelTesting.js
@@ -1,2 +1,27 @@
+import { isTypedArray } from "util/types";
+const { expectRevert, expectEvent, BN } = require('@openzeppelin/test-helpers');
 const NFTDuels = artifacts.require('NFTDuels');
 const NFT = artifacts.require('NFT');
+
+contract( 'Market' , () => {
+    let market;
+    let token;
+
+    const tokenId = 1;
+
+    before( () => {
+        market = await NFTDuels.deployed();
+        token = await NFT.deployed();
+
+        await token.mint();
+    })
+
+    describe( 'Escrow Token', () =>{
+        it('should prevent escrowing token' , () =>{
+            expectRevert(market.escrowToken(
+                token.address,
+                tokenId
+            ), 'ERC721: transfer caller is not owner nor approved');
+        })
+    })
+});
